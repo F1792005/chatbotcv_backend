1c7bc7d5a079f1d8efdc2245afd08b231ee5329a
NHP_DE <fong1792005@gmail.com>
2025-11-06 13:09:33 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ app.add_middleware(
         "http://localhost:5500",
         "http://127.0.0.1:8000",
         "http://localhost:8000",
-        "https://f1792005.github.io/chatbotcv_frontend/"
+        "https://f1792005.github.io"
     ],
     allow_credentials=True,
     allow_methods=["*"],
